5facd40693623f467d0f4924a22de9c706009e98
haki9975 <elias.robert.hakim@gmail.com>
2021-06-22 14:16:56 -0400

    Migrate Body to Comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_21_221039) do
+ActiveRecord::Schema.define(version: 2021_06_22_181557) do
 
   create_table "comments", force: :cascade do |t|
     t.string "username"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2021_06_21_221039) do
     t.integer "post_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "body"
     t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
