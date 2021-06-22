93897e938352b1e40cf73bf1e2a39d08de12de82
haki9975 <elias.robert.hakim@gmail.com>
2021-06-22 18:29:00 -0400

    Add comment attributes to post strong params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -46,6 +46,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:username, :likes, :image, :date)
+      params.require(:post).permit(:username, :likes, :image, :date, comment_attributes:[:id, :username, :likes, :date, :post_id])
     end
 end
